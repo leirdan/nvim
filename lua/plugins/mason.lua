44d95f31bf4721b165d740a2ac86042b4932db5a
leirdan <uouleirdan@gmail.com>
2024-05-16 22:21:00 -0300

    :sparkles: feat: updated lsp servers

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = { "asm_lsp", "clangd", "ltex", "tailwindcss", "tsserver", "lua_ls" }
+        ensure_installed = { "clangd", "jdtls", "ltex", "tailwindcss", "tsserver", "lua_ls" }
       })
     end
   },
@@ -18,11 +18,11 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.asm_lsp.setup({})
+      lspconfig.jdtls.setup({})
       lspconfig.clangd.setup({})
       lspconfig.ltex.setup({})
       lspconfig.tsserver.setup({})
-      vim.keymap.set('n', '<C-i>', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<C-h>', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<C-c>', vim.lsp.buf.code_action, {})
       vim.keymap.set('n', '<C-d>', vim.lsp.buf.definition, {})
     end
